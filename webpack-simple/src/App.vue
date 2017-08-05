45438483ee37f36a4fe0b206b4656876522ed139
Enrique Garcia Navalon <garcianavalon@gmail.com>
2017-08-05 13:00:15 +0200

    very basic responsive grid

--- webpack-simple/src/App.vue
+++ webpack-simple/src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="pure-g">
+    <div class="pure-u-1 pure-u-md-1-4"><img class="logo" src="./assets/botijo.gif"></div>
+    <div class="pure-u-1 pure-u-md-3-4"><h1>{{ msg }}</h1></div>
+    <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">A</div>
+    <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">B</div>
+    <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">C</div>
+    <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">D</div>
+    <div class="pure-u-1">FOOTER</div>
+
   </div>
 </template>
 
@@ -24,37 +16,14 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Vue.js + Pure.css bundled with Webpack'
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.logo {
+  width: 50px;
 }
 </style>
--- webpack-simple/src/main.js
+++ webpack-simple/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'purecss'
+import 'purecss/build/grids-responsive-min.css'
 
 new Vue({
   el: '#app',
